996f68a64113f259b018a5168ff0cf61f4f68104
Aurél Gábris <gabris.aurel@fjfi.cvut.cz>
2019-09-13 20:15:31 +0200

    Mapping function completed.

--- structure.py
+++ structure.py
@@ -8,9 +8,9 @@ from qiskit.tools.visualization import plot_state_city
 from qiskit.providers.aer import StatevectorSimulator
 
 
-
-# Code to apply a particular gate
-
+# Size of the gameboard
+boardh = 2
+boardw = 2
 
 def apply_operation(state_vector, qc_op):
     # state vector is an array of four complex numbers
@@ -28,6 +28,37 @@ def apply_operation(state_vector, qc_op):
     result = execute(circ, simulator).result()
     return  result.get_statevector(circ);
 
+#we need a dictionary
+mapping = ( ([0,0,0,0],0),
+            ([1, 0, 0, 0],1), ([0, 1, 0, 0],2),
+            ([1, 0, 0, 0],3), ([1, 0, 0, 0],4) )
+
+def find_match(state_vector):
+    state_vector=np.conjugate(state_vector)
+    for i in mapping:
+        if np.abs( np.dot(state_vector,i[0]) ) > 0.9:
+            return(i[1])
+        
+    # if no match found, return the next integer
+    return len(mapping)
+
+
+def shoot_state(sv, column, board_vector, board_map):
+    # sv: np.array object describing the state vector shot
+    # column: column where to apply it
+    # board_vector: array holding the quantum states at each location
+    # board_map: array holding the indices for the game-mechanics
+    
+    return;
+
+def shoot_operator(qc_op, column, board_vector, board_map):
+    # qc_op: QuantumCircuit object describing the operation to be done
+    # column: column where to apply it
+    # board_vector: array holding the quantum states at each location
+    # board_map: array holding the indices for the game-mechanics
+    
+    return;
+
 
 # QC implementing a particular operator the player can use
 qc_h1 = QuantumCircuit(2)
@@ -37,10 +68,15 @@ qc_x1 = QuantumCircuit(2)
 qc_x1.x(0)
 
 
-statevector= [ 1, 0, 0, 0]
+board_vector=np.zeros( (boardh, boardw, 4), dtype=complex)
+board_map=np.full( (boardh, boardw, 4), np.nan)
 
-statevector = apply_operation(statevector, qc_x1)
+board_vector[0,0]= np.array([ 1, 0, 0, 0])
+
+statevector = apply_operation(board_vector[0,0], qc_x1)
 print(statevector)
 
 statevector = apply_operation(statevector, qc_h1)
 print(statevector)
+
+print(board_vector)

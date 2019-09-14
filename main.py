0579e0cdcadf771dda0965587fe785eca7763dcf
Carolin Gold <cgold@phys.ethz.ch>
2019-09-14 11:50:28 +0200

    implemented the shooting of operators and states

--- main.py
+++ main.py
@@ -37,28 +37,34 @@ class mainWindow(object):
         self.value=self.e.get()
         self.master.destroy()
 
+  
 #st.debug_structure()
 #Bubble_array,state_array=pa.debug_python_array()
 
-#initialize Bubble_array and state_array
+#initialize Bubble_array, state-array and figure
 #------------------------------------------------------------------------------
 (nh,nw)=(10,10)
 Bubble_array=np.empty((nh,nw,))
 Bubble_array[:]=np.nan#np.zeros(100).reshape(10,10)
 state_array=np.zeros( (nh, nw, 4), dtype=complex)
 
+fig, ax, my_cmap = pa.init_display_array(Bubble_array, nh, nw)
+plt.pause(0.0001)
+
 #add a row
 #------------------------------------------------------------------------------
 Bubble_array, state_array=pa.Add_new_row(Bubble_array, state_array)
-
-plt.figure()
-plt.imshow(Bubble_array)
-plt.show()
 
 # check for clusters
 #------------------------------------------------------------------------------
 Bubble_array, state_array=pa.remove_clusters(Bubble_array, state_array)#,isdebug=True)
 
+pa.update_display_array(fig,ax,my_cmap,Bubble_array, nh, nw)
+'''
+plt.figure()
+plt.imshow(Bubble_array,plt.cm.jet)
+plt.show()
+'''
 # get user input about where to shoot the object
 #------------------------------------------------------------------------------
 shooting_element=pa.shooting_element()
@@ -75,6 +81,5 @@ row=pa.index_1st_notNan_entry_fromBotttom(Bubble_array,User_input)
 print('row',row)
 Bubble_array,state_array=st.shoot_shooting_element(shooting_element[0],shooting_element[1],row,User_input,Bubble_array,state_array,)
 
-plt.figure()
-plt.imshow(Bubble_array)
-plt.show()
+pa.update_display_array(fig,ax,my_cmap,Bubble_array, nh, nw)
+plt.pause(1000)
